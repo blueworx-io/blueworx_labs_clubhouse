3520b3a0cab616194b794c87f2205fadcabe61f6
blueworx-io <luke.mcfarland@babyblue.info>
2026-08-13 16:50:02 +0200

    feat: add the page composition store

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -31,6 +31,7 @@ require_once __DIR__ . '/blocks/class-block-context.php';
 require_once __DIR__ . '/blocks/class-block-types.php';
 require_once __DIR__ . '/blocks/class-block-addresses.php';
 require_once __DIR__ . '/blocks/class-block-library.php';
+require_once __DIR__ . '/blocks/class-page-composition.php';
 
 // Theme
 require_once __DIR__ . '/theme/interface-base-look.php';
